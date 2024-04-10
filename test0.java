a9488d15b1751d6223c46751932a318bf674f55e
SanjayUG <ganjekarsanjay6@gmail.com>
2024-04-10 11:32:34 +0530

    added .java & .js

---
+++ test0.java
@@ -0,0 +1,5 @@
+class test0 {
+    public static void main(String[] args) {
+        System.out.println("HARE KRISHNA HARE KRISHNA KRISHNA KRISHNA HARE HARE, HARE RAM HARE RAM RAM RAM HARE HARE");
+    }
+}
---
+++ test1.js
@@ -0,0 +1,13 @@
+console.log("HARE KRISHNA");
+let obj = {
+    nums,
+    ch,
+    bools
+}
+console.log(obj);
+
+let fun = () => {
+    console.log("Hello World");
+}
+
+fun;
