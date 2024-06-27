9e7fb4c00c12db80df485646a7e2f4ef746e5037
SanjayUG <ganjekarsanjay6@gmail.com>
2024-06-27 20:47:53 +0530

    created bulb on/off

---
+++ A_Bulb/app.js
@@ -0,0 +1,7 @@
+function bulbOn() {
+    document.getElementById("bulb").src = "/images/bulbOnImg.png";
+}
+
+function bulbOff() {
+    document.getElementById("bulb").src = "/images/bulbOffImg.jpg";
+}
