4a2b5532ef135c4457a167fb6651ccf014ad07ba
SanjayUG <ganjekarsanjay6@gmail.com>
2024-07-06 17:03:35 +0530

    feat: added random color generator

--- QR_Code_generator/app.js
+++ QR_Code_generator/app.js
@@ -21,14 +21,49 @@ let mode = "light";
 
 modeBtn.addEventListener("click", () => {
     if (mode === "light") {
-        body.classList.add("dark");
-        body.classList.remove("light");
+        enableDarkMode();
         modeBtn.textContent = "Click to Light Mode";
         mode = "dark";
     } else {
-        body.classList.add("light");
-        body.classList.remove("dark");
+        enableLightMode();
         modeBtn.textContent = "Click to Dark Mode";
         mode = "light";
     }
+});
+
+// Enable Dark Mode
+function enableDarkMode() {
+    body.style.backgroundColor = "#333333";
+    body.style.color = "#ffffff";
+    document.querySelector('h1').style.color = "#ffffff";
+    document.querySelector('h3').style.color = "#ffffff";
+    document.querySelector('.container').style.backgroundColor = "#444444";
+    document.querySelector('.container').style.boxShadow = "0 0 10px rgba(255,255,255,0.1)";
+    document.querySelector('input[type="text"]').style.backgroundColor = "#555555";
+    document.querySelector('input[type="text"]').style.color = "#ffffff";
+    document.querySelector('input[type="text"]').style.border = "1px solid #777777";
+    document.querySelector('button').style.backgroundColor = "#666666";
+}
+
+// Enable Light Mode
+function enableLightMode() {
+    body.style.backgroundColor = "#ffffff";
+    body.style.color = "#000000";
+    document.querySelector('h1').style.color = "#000000";
+    document.querySelector('h3').style.color = "#ede4e4";
+    document.querySelector('.container').style.backgroundColor = "#2bb1ff";
+    document.querySelector('.container').style.boxShadow = "0 0 10px rgba(0,0,0,0.1)";
+    document.querySelector('input[type="text"]').style.backgroundColor = "#ffffff";
+    document.querySelector('input[type="text"]').style.color = "#000000";
+    document.querySelector('input[type="text"]').style.border = "1px solid #ccc";
+    document.querySelector('button').style.backgroundColor = "#1928ae";
+    document.querySelector('button').style.color = "#ffffff";
+}
+
+// Random background
+let randomBtn = document.querySelector('#random');
+
+randomBtn.addEventListener("click", () => {
+    let randomGen = "#" + Math.floor(Math.random() * 16777215).toString(16); // 16777215 is the decimal equivalent of FFFFFF
+    document.body.style.backgroundColor = randomGen;    
 });
